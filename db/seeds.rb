8f6003112f06705d3970fb57787f325f255c45ec
Maxim (Fire Sparkle) <max_fedotov97@mail.ru>
2020-06-15 19:06:09 +0700

    Version 0.4.1.2
    - Added one more field to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,5 @@
 #  event.color = ['black','green','red', nil].sample
 #  event.save
 #end
-	User.create( name: 'Test', email: 'test@mail.ru', admin: 1, group_id: 1 )
-	User.create( name: 'User', email: 'user@mail.ru', admin: 0, group_id: 2 )
+	User.create( name: 'Test', email: 'test@mail.ru', admin: 1, password_digest: '$2a$12$1ZpP2d34YGPlS5hvMWee3euBzVOSIsTsL4Fw0yKr6jEer3Y28E26O', group_id: 1 )
+	User.create( name: 'User', email: 'user@mail.ru', admin: 0, password_digest: '$2a$12$mjUvKwlSSr7wKPrxYHFV3.b4/sbjDRGQs8w6RQSwYDIOZbAJPnSi6', group_id: 2 )
